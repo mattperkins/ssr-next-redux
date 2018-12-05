bb26b581ba3c50ae844d17574c3ded1ea729aa4c
mattperkins <github@mattperkins.com>
2018-12-05 04:07:40 +0000

    revert to pre redux

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import './css/reset.css'
 import './index.css'
-import './Block/' // importing css from this dir
+import './Block' // importing css from this dir
 import * as React from 'react'
 import data from '../data/data.json'
-import Card from './Card/'
+import Card from './Card'
 
 export default class Index extends React.Component {
   static async getInitialProps () {
-    return { cardz: data }
+    return { cards: data }
   }
   render () {
     return (
       <div>
-        {this.props.cardz.map(card => (
+        {this.props.cards.map(card => (
           <Card
             key={card.id}
             description={card.description}
