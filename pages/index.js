fac1d6eae1f7f9cb009c82e06be1ca3e6b88b25f
mattperkins <github@mattperkins.com>
2018-12-05 02:11:16 +0000

    import Card component

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import './index.css'
 import './Block/' // importing css from this dir
 import * as React from 'react'
 import data from '../data/data.json'
-
-const Card = ({ description }) => <p>{description}</p>
+import Card from './Card/'
 
 export default class Index extends React.Component {
   static async getInitialProps () {
